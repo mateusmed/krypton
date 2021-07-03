d8744b1dba31f2c71c97b83dbcce3af88a59a36e
Mateus Med <mateus.medeiros46@gmail.com>
2021-07-03 17:14:39 -0300

    commit  to save

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ pm2 ls
 pm2 delete -s krypton || :
 pm2 start index.js --name krypton -f
 pm2 save
+
+
+cd rig-brain-front/
+npm install
+
+pm2 ls
+pm2 delete -s krypton-frontend || :
+pm2 start npm --name krypton-frontend -f -- run dev
+pm2 save
